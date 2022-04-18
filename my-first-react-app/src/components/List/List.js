23bf87167f1aea649870d7d08547fe62414f283b
Łukasz Nowak <enowuigrek@gmail.com>
2022-04-18 11:47:44 +0200

    add useState - 11.6

--- my-first-react-app/src/components/List/List.js
+++ my-first-react-app/src/components/List/List.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 
 const List = () => {
+
+  const [columns, setColumns] = useState([
+      { id: 1, title: 'Books', icon: 'book' },
+      { id: 2, title: 'Movies', icon: 'film' },
+      { id: 3, title: 'Games', icon: 'gamepad' }
+  ]);
+
+  setTimeout(() => {
+    setColumns([...columns, { id: 4, title: 'Test column'}]);
+  }, 1000000);
+
+  console.log(columns);
+
   return (
     <div>
       <header className={styles.header}>
@@ -9,9 +23,7 @@ const List = () => {
       </header>
       <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
-        <Column icon="book" title="Books"/>
-        <Column icon="film" title="Movies"/>
-        <Column icon="gamepad" title="Games"/>
+        {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
       </section>
     </div>
   );
